c1973e6e068665ef74d392dafb16b683c1aa769c
Faiaz Riasat Saad <saadfaiaz35@gmail.com>
2025-05-05 02:57:20 +0600

    aa

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/spam-email-detector/', // 👈 change to your repo name
+  base: '/spam-email-detector/',
   plugins: [react()]
 });
