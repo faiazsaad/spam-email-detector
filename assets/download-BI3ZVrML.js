cd5c0655f72cf10ab74f9fad44ef62935fc32c50
Faiaz Riasat Saad <saadfaiaz35@gmail.com>
2025-05-05 02:37:20 +0600

    Updates

--- assets/download-BI3ZVrML.js
+++ assets/download-BI3ZVrML.js
@@ -1,4 +1,4 @@
-import{c as o}from"./index-lUf04x_u.js";/**
+import{c as o}from"./index-CyulkRTN.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
